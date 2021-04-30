5f4844f9056bd9342414c6f25fb0ac8fbfd90911
hashika <m.g.hashikamaduranga@gmail.com>
2021-04-30 09:35:25 +0530

    changes

--- Uk_backend/src/services/NdaService.js
+++ Uk_backend/src/services/NdaService.js
@@ -1,9 +1,9 @@
-const NdaService = (event)=>{
-    return {
-        body:JSON.stringify({
-            DATA:"HELLO WORLD"
-        })
-    }
-}
+const NdaService = (event) => {
+  return {
+    body: JSON.stringify({
+      DATA: "HELLO WORLD",
+    }),
+  };
+};
 
 module.exports = NdaService;
